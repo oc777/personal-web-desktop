0b7d19671315160ce01c9c921e8a7de749b5cca6
oc777 <oc222ba@student.lnu.se>
2020-04-19 22:08:08 +0200

    saving todo items to local storage

--- src/js/todo.js
+++ src/js/todo.js
@@ -10,35 +10,38 @@
 class Todo {
   constructor (element) {
     this.el = element
-    this.todos = []
-    this.todo = {}
+    this.todoLists = []
+    this.todoItems = []
     this.todoListId = undefined
   }
 
   init () {
-    console.log('Hello world')
+    // press enter to create new list
     const input = this.el.querySelector('input')
     input.addEventListener('keydown', e => {
       this.getInput(input, e, this.addToList.bind(this))
     })
 
+    // listen to clicks on all elements
     this.el.addEventListener('click', e => {
       console.log(e.target)
       if (e.target.parentNode.nodeName === 'OL') {
+        // ol - todo lists
         console.log('todo lists')
         this.openList(e.target.id, e.target.innerText)
       } else if (e.target.parentNode.nodeName === 'UL') {
+        // ul - todo items in a specific list
         console.log('todo items')
       }
     })
-    // const list = this.el.querySelector('ol')
-    // list.addEventListener('click', e => this.openList(e))
 
-    if (window.localStorage.getItem('todos') !== null) {
-      this.todos = this.getAllTodoLists()
+    // render lists if saved
+    if (window.localStorage.getItem('todoLists') !== null) {
+      this.todoLists = this.getAllTodoLists()
     }
   }
 
+  // get text from input field
   getInput (input, e, callback) {
     if (e.keyCode === 13 && input.value.length > 1) {
       const msg = input.value
@@ -49,8 +52,9 @@ class Todo {
     }
   }
 
+  // add item as a list or as todo
   addToList (msg, e) {
-    console.log(msg)
+    // console.log(msg)
     if (e.target.name === 'title') {
       this.addNewList(msg)
     } else if (e.target.name === 'todo') {
@@ -58,31 +62,31 @@ class Todo {
     }
   }
 
+  // render new list and save to storage
   addNewList (title) {
-    console.log(title)
-    // const list = this.el.querySelector('ol')
-    // const li = document.createElement('li')
-    // li.textContent = title
-    // list.prepend(li)
+    // console.log(title)
     this.printListEl('ol', title)
     const todoList = {
       id: Date.now(),
       title: title
     }
-    this.todos.push(todoList)
-    window.localStorage.setItem('todos', JSON.stringify(this.todos))
+    this.todoLists.push(todoList)
+    window.localStorage.setItem('todoLists', JSON.stringify(this.todoLists))
   }
 
+  // render a LI element
   printListEl (parent, txt, id) {
     const list = this.el.querySelector(parent)
     const li = document.createElement('li')
 
+    // list name
     if (parent === 'ol') {
       li.setAttribute('id', id)
       li.textContent = txt
       list.prepend(li)
     }
 
+    // todo item
     if (parent === 'ul') {
       const tempTodo = this.el.querySelector('#todo-item')
       const todoItem = document.importNode(tempTodo.content, true)
@@ -92,36 +96,44 @@ class Todo {
     }
   }
 
+  // render new todo item and add to storage
   addNewTodo (todo) {
-    console.log(todo)
+    // console.log(todo)
     this.printListEl('ul', todo)
     const todoItem = {
       id: Date.now(),
       title: todo,
       status: 'pending'
     }
-    this.todo = todoItem
-    window.localStorage.setItem(this.todoListId, this.todo)
+    this.todoItems.push(todoItem)
+    console.log(this.todoItems)
+    window.localStorage.setItem(this.todoListId, JSON.stringify(this.todoItems))
   }
 
+  // get lists from storage and render
   getAllTodoLists () {
-    const todos = window.localStorage.getItem('todos')
-    console.log(JSON.parse(todos))
-    const lists = JSON.parse(todos)
+    const todoLists = window.localStorage.getItem('todoLists')
+    const lists = JSON.parse(todoLists)
+    console.log(lists)
 
     lists.forEach(list => this.printListEl('ol', list.title, list.id))
 
     return lists
   }
 
+  // get todo items from storage for specific List id
+  // render new view
   openList (id, title) {
     this.todoListId = id
-    if (window.localStorage.getItem('id') === null) {
+    this.todoItems = []
+    if (window.localStorage.getItem(id) === null) {
       window.localStorage.setItem(id, '')
     } else {
-      this.todo = window.localStorage.getItem(id)
+      const todos = window.localStorage.getItem(id)
+      this.todoItems = JSON.parse(todos)
     }
 
+    // render view
     this.el.querySelector('.lists').style.display = 'none'
     const todoPage = this.el.querySelector('.todos')
     const tempTodo = this.el.querySelector('#todo')
@@ -130,6 +142,14 @@ class Todo {
     todoItems.querySelector('h1').textContent = title
     todoPage.appendChild(todoItems)
 
+    // if items already exist
+    if (todoItems.length !== 0) {
+      this.todoItems.forEach(todo =>
+        this.printListEl('ul', todo.title, todo.id)
+      )
+    }
+
+    // listen to input
     const input = this.el.querySelector('input[name=todo]')
     input.addEventListener('keydown', e => {
       this.getInput(input, e, this.addToList.bind(this))
