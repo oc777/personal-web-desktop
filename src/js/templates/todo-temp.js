5e7e7b8d8cd9f9e56959e57500a2b20a503366a2
oc777 <oc222ba@student.lnu.se>
2020-04-19 17:48:07 +0200

    adding todo base

--- src/js/templates/todo-temp.js
+++ src/js/templates/todo-temp.js
@@ -1,12 +1,28 @@
 module.exports = `
-<div class="chat-window">
+<div class="todo-window">
     <div class="header">
         <span class="fa"><i class="far fa-check-square"></i></span>
         <span class="fa close"><i class="far fa-times-circle"></i></span>
     </div>
     <div class="body">
-        <div class="conversation"></div>
-        <div class="msg"></div>
+        <div class="lists">
+            <h1>My TODO lists</h1>
+            <input type="text" name="title" placeholder="Add new list name & hit enter"></input>
+            <ol></ol>
+        </div>
+        <div class="todos"></div>
     </div>
+    <template id="todo">
+        <h1></h1>
+        <input type="text" name="todo" placeholder="Add new TODO & hit enter"></input>
+        <ul></ul>
+    </template>
+    <template id="todo-item">
+        <li>
+            <i class="far fa-circle"></i>
+            <p></p>
+            <i class="far fa-times-circle"></i>
+        </li>
+    </template>
 </div>
 `
---
+++ src/js/todo.js
@@ -0,0 +1,67 @@
+/**
+ * Module for TODO app
+ * @author Olga Christensen
+ */
+
+/**
+ * TODO app
+ * @constructor
+ */
+class Todo {
+  constructor (element) {
+    this.el = element
+    this.todos = []
+    this.todo = {}
+  }
+
+  init () {
+    console.log('Hello world')
+    const input = this.el.querySelector('input')
+    input.addEventListener('keydown', e => {
+      this.getInput(input, e, this.addToList.bind(this))
+    })
+
+    const list = this.el.querySelector('ol')
+    list.addEventListener('click', e => this.openList(e))
+
+    if (window.localStorage.getItem('todos') !== null) this.getAllTodos()
+  }
+
+  getInput (input, e, callback) {
+    if (e.keyCode === 13 && input.value.length > 1) {
+      const msg = input.value
+      e.preventDefault()
+      input.value = ''
+      console.log(e.target.name)
+      callback(msg, e)
+    }
+  }
+
+  addToList (msg, e) {
+    console.log(msg)
+    if (e.target.name === 'title') {
+      this.addNewList(msg)
+    } else if (e.target.name === 'todo') {
+      this.addNewTodo(msg)
+    }
+  }
+
+  addNewList (title) {
+    console.log(title)
+    const list = this.el.querySelector('ol')
+    const li = document.createElement('li')
+    li.textContent = title
+    list.prepend(li)
+    const todoList = {
+      id: 1,
+      title: title
+    }
+  }
+
+  addNewTodo (todo) {
+    console.log(todo)
+  }
+}
+// <i class="far fa-check-circle"></i>
+
+module.exports = Todo
