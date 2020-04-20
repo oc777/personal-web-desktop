489d477a120d4fc2db1d6504738d79b3659b22ed
oc777 <oc222ba@student.lnu.se>
2020-04-20 14:22:48 +0200

    adding memory init

---
+++ src/js/memory.js
@@ -0,0 +1,40 @@
+/**
+ * Module for Memory app
+ * @author Olga Christensen
+ */
+
+/**
+ * TODO app
+ * @constructor
+ */
+class Memory {
+  constructor (element, rows, cols) {
+    this.el = element
+    this.rows = rows || 4
+    this.cols = cols || 4
+    this.board = 0
+    this.bricks = []
+    this.collection = ['atom', 'bath', 'bug', 'code', 'coffee', 'globe', 'keyboard', 'robot', 'terminal', 'rocket']
+    this.turned1 = undefined
+    this.turned2 = undefined
+    this.gameDiv = undefined
+  }
+
+  init () {
+    console.log('game on')
+    this.gameDiv = this.el.querySelector('.game')
+    this.drawBoard()
+  }
+
+  drawBoard () {
+    this.gameDiv.classList.add(`sq${this.rows}`)
+    const brickTemp = this.el.querySelector('#brick-temp')
+    this.board = this.rows * this.rows
+    for (let i = 0; i < this.board; i++) {
+      const node = document.importNode(brickTemp.content, true)
+      this.gameDiv.appendChild(node)
+    }
+  }
+}
+
+module.exports = Memory
