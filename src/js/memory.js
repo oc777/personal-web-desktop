f7ae53940db189e8be1695ee0acc1cb31d403a16
oc777 <oc222ba@student.lnu.se>
2020-04-20 18:57:48 +0200

    flipping the bricks back or showing a match

--- src/js/memory.js
+++ src/js/memory.js
@@ -12,7 +12,7 @@ class Memory {
     this.el = element
     this.rows = rows || 4
     this.cols = cols || 4
-    this.board = 0
+    this.boardSize = 16
     this.bricks = []
     this.collection = [
       'atom', 'bath', 'bug', 'code', 'coffee', 'meteor',
@@ -22,6 +22,7 @@ class Memory {
     this.turned1 = undefined
     this.turned2 = undefined
     this.gameDiv = undefined
+    this.isClickable = true
   }
 
   init () {
@@ -40,35 +41,42 @@ class Memory {
 
   clickHandler (event) {
     const brickIndex = parseInt(event.target.closest('.brick').getAttribute('data-brickIndex'))
-    let icon
+    console.log(this.isClickable)
+    // check if target element or its parent has 'data-brickIndex'
+    // check if the pair match is in progress
+    if (Number.isInteger(brickIndex) && this.isClickable) {
+      let icon
+      // get the icon element
+      switch (event.target.nodeName) {
+        case 'DIV':
+          icon = event.target.firstElementChild.firstElementChild
+          break
+        case 'A':
+          icon = event.target.firstElementChild
+          break
+        default:
+          icon = event.target
+      }
+      // console.log(event.target)
+      // console.log(icon)
 
-    switch (event.target.nodeName) {
-      case 'DIV':
-        icon = event.target.firstElementChild.firstElementChild
-        break
-      case 'A':
-        icon = event.target.firstElementChild
-        break
-      default:
-        icon = event.target
+      this.turnBrick(brickIndex, icon)
     }
-    // console.log(event.target)
-    // console.log(icon)
-
-    this.turnBrick(brickIndex, icon)
   }
 
+  // dynamically draw the board
   drawBoard () {
     this.gameDiv.classList.add(`sq${this.rows}`)
     const brickTemp = this.el.querySelector('#brick-temp')
-    this.board = this.rows * this.rows
-    for (let i = 0; i < this.board; i++) {
+    this.boardSize = this.rows * this.rows
+    for (let i = 0; i < this.boardSize; i++) {
       const node = document.importNode(brickTemp.content, true)
       node.firstElementChild.setAttribute('data-brickIndex', i)
       this.gameDiv.appendChild(node)
     }
   }
 
+  // shuffles array element
   shuffleArray (array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -76,30 +84,75 @@ class Memory {
     }
   }
 
+  // create an array of icons/bricks for the game
   selectBricks () {
+    // shuffle the collection
     this.shuffleArray(this.collection)
-    for (let i = 0; i < this.board / 2; i++) {
+    // grab pairs of icons/bricks from collection
+    // to this game selection
+    for (let i = 0; i < this.boardSize / 2; i++) {
       this.bricks.push(this.collection[i])
       this.bricks.push(this.collection[i])
     }
+    // shuffle this game bricks
     this.shuffleArray(this.bricks)
   }
 
+  // turn the clicked brick
+  // index of the brick in game bricks array
+  // i element that was clicked
   turnBrick (index, el) {
+    // show the icon
     el.setAttribute('class', `fas fa-${this.bricks[index]}`)
 
+    // check for pair
     if (!this.turned1) {
-      this.turned1 = el.getAttribute('class')
+      // first brick turned
+      this.turned1 = el
       console.log(this.turned1)
     } else {
-      this.turned2 = el.getAttribute('class')
+      // same el was clicked twice
+      if (this.turned1 === el) return
+
+      // second brick turned
+      this.turned2 = el
       console.log(this.turned2)
 
-      if (this.turned1 === this.turned2) {
+      // disable clicks on the board
+      this.isClickable = false
+      // check for a match
+      if (this.turned1.getAttribute('class') === this.turned2.getAttribute('class')) {
         console.log('Pair!')
+        // mark the pair
+        window.setTimeout(() => {
+          // <i class="fas fa-trophy"></i>
+
+          // change the icon to 'trophy' on both bricks
+          this.turned1.setAttribute('class', 'fas fa-trophy')
+          this.turned2.setAttribute('class', 'fas fa-trophy')
+          // remove 'brickIndex' from both bricks to prevent clicking
+          this.turned1.closest('.brick').removeAttribute('data-brickIndex')
+          this.turned2.closest('.brick').removeAttribute('data-brickIndex')
+          // prepare for the next pair
+          this.turned1 = undefined
+          this.turned2 = undefined
+
+          // enable clicks on the board
+          this.isClickable = true
+        }, 500)
       } else {
-        this.turned1 = undefined
-        this.turned2 = undefined
+        // not a pair
+        window.setTimeout(() => {
+          // turn the brick around again
+          this.turned1.setAttribute('class', 'fas fa-question')
+          this.turned2.setAttribute('class', 'fas fa-question')
+          // prepare for the next pair
+          this.turned1 = undefined
+          this.turned2 = undefined
+
+          // enable clicks on the board
+          this.isClickable = true
+        }, 500)
       }
     }
   }
