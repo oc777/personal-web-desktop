4179ed720f0e5dba12cb6703e2260ac1e6a3c561
oc777 <oc222ba@student.lnu.se>
2020-04-21 12:26:17 +0200

    adding option to quit open app

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -30,14 +30,23 @@ class Desktop {
     this.chatOffsetY = 30
     this.memoryOffsetY = 30
     this.todoOffsetY = 30
+
+    this.chatWindows = []
+    this.memoryWindows = []
+    this.todoWindows = []
+
+    this.contextMenu = document.getElementById('context-menu')
+    this.quitAppLink = document.getElementById('quit-app')
+    this.contextMenuVisible = false
   }
 
   /**
    * enable launching apps from Dock on click
-   * @todo add 'quit app'
    */
   initDock () {
     const dock = document.getElementById('dock')
+
+    // events to launch apps
     dock.addEventListener('click', event => {
       if (event.target.closest('#chat')) {
         this.getXY('chat')
@@ -52,6 +61,74 @@ class Desktop {
         this.launch('todo', this.todoId++, this.todoX + this.todoOffsetX, this.todoY + this.todoOffsetY)
       }
     })
+
+    // events to apps context menu
+    dock.addEventListener('contextmenu', e => {
+      e.preventDefault()
+      if (e.target.closest('#chat')) {
+        this.showOptions('chat', e)
+      }
+      if (e.target.closest('#memory')) {
+        this.showOptions('memory', e)
+      }
+      if (e.target.closest('#todo')) {
+        this.showOptions('todo', e)
+      }
+    })
+
+    // event to close context menu
+    window.addEventListener('click', e => {
+      if (this.contextMenuVisible && !e.target.closest('#context-menu')) {
+        this.contextMenu.style.display = 'none'
+        this.contextMenuVisible = false
+      }
+    })
+
+    // event to quit app
+    this.quitAppLink.addEventListener('click', this.quitAppHandler.bind(this))
+  }
+
+  /**
+   * open context menu to quit app
+   * @param {string} app - app name o quit
+   * @param {event} e - click event
+   */
+  showOptions (app, e) {
+    // const menu = document.getElementById('context-menu')
+    if (this[app + 'Windows'].length > 0) {
+      this.contextMenu.setAttribute('data-app', app)
+      const left = e.pageX
+      const top = e.pageY - 30
+      this.contextMenu.style.left = `${left}px`
+      this.contextMenu.style.top = `${top}px`
+      this.contextMenu.style.display = 'block'
+      this.quitAppLink.textContent = `Quit ${app}`
+      this.contextMenuVisible = true
+    }
+  }
+
+  /**
+   * Event handler to quit app
+   * @param {event} e - click on context menu
+   */
+  quitAppHandler (e) {
+    // get app name to quit
+    const app = e.target.parentNode.attributes['data-app'].value
+
+    // remove each open app div
+    this[app + 'Windows'].forEach(i => {
+      i.el.remove()
+    })
+
+    // reset app values
+    this[app + 'Windows'] = []
+    this[app + 'Id'] = 0
+    this[app + 'OffsetX'] = 30
+    this[app + 'OffsetY'] = 30
+
+    // close menu
+    this.contextMenu.style.display = 'none'
+    this.contextMenuVisible = false
   }
 
   /**
@@ -64,6 +141,7 @@ class Desktop {
   launch (app, id, x, y) {
     const w = new DTW(id, x, y)
     w.openWindow(app)
+    this[app + 'Windows'].push(w)
   }
 
   /**
@@ -75,10 +153,6 @@ class Desktop {
   getXY (app) {
     const wh = window.innerHeight
     const ww = window.innerWidth
-    // console.log(`width: ${ww}`)
-    // console.log(`height: ${wh}`)
-    // console.log(`offsetX: ${this[app + 'OffsetX']}`)
-    // console.log(`offsetY: ${this[app + 'OffsetY']}`)
 
     // if app window gets too close to browser's right edge
     if (this[app + 'OffsetX'] + this[app + 'X'] > ww - 200) {
